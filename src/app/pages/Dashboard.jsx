2c6a3f985a5312116b002d41165f197f4f5c2e6c
LidiaKovac <lidia.kovac1998@gmail.com>
2021-02-13 18:58:29 +0100

    adds icons

--- src/app/pages/Dashboard.jsx
+++ src/app/pages/Dashboard.jsx
@@ -4,11 +4,15 @@ import "../../styling/Dashboard.scss";
 import "../../App.scss";
 import moment from "moment";
 import timestamp from 'unix-timestamp'
-import { FaTemperatureHigh } from "react-icons/fa";
 import { connect } from "react-redux";
 import { ThemeContext, Themes } from "../../context/theme";
 import Search from "../components/Search";
 import Loader from "../components/Loader";
+import Snow from '../../assets/snow.png'
+import Cloud from '../../assets/cloud.png'
+import Rain from '../../assets/rain.png'
+import Sun from '../../assets/sun.png'
+import Thunder from '../../assets/thunder.png'
 
 const mapStateToProps = (state) => state;
 const mapDispatchToProps = (dispatch) => ({
@@ -46,10 +50,8 @@ const Dashboard = (props) => {
     (await coords) && (await props.get_coords(coords.lat, coords.lon));
 
     let response = await fetch(
-      ` https://api.openweathermap.org/data/2.5/onecall?exclude=alerts&units=metric&lat=${
-        coords ? coords.lat : "45.4643"
-      }&lon=${coords ? coords.lon : "9.1895"}&appid=${
-        process.env.REACT_APP_API_KEY
+      ` https://api.openweathermap.org/data/2.5/onecall?exclude=alerts&units=metric&lat=${coords ? coords.lat : "45.4643"
+      }&lon=${coords ? coords.lon : "9.1895"}&appid=${process.env.REACT_APP_API_KEY
       }`
     );
     let weather = await response.json();
@@ -82,87 +84,115 @@ const Dashboard = (props) => {
       {loading ? (
         <Loader />
       ) : (
-        <div className="weather-wrap">
-          <h1 className='cabin mb-3'>Weather in {capitalize(props.city.city )}:</h1>
-          <div className="d-flex flex-row">
-            {props.weather.data.daily &&
-              props.weather.data.daily.map((curr) => (
-                  
-                <>
-                
-                  <div
-                    className={
-                      curr &&
-                      (curr.weather[0].id >= 200 && curr.weather[0].id < 300
-                        ? "weather-inner-wrap m-2 thunder"
-                        : curr.weather[0].id >= 300 && curr.weather[0].id < 400
-                        ? "weather-inner-wrap m-2 drizzle"
-                        : curr.weather[0].id >= 400 && curr.weather[0].id < 600
-                        ? "weather-inner-wrap m-2 rain"
-                        : curr.weather[0].id >= 600 && curr.weather[0].id < 700
-                        ? "weather-inner-wrap m-2 snow"
-                        : curr.weather[0].id === 800
-                        ? "weather-inner-wrap m-2 clear"
-                        : curr.weather[0].id > 800
-                        ? "weather-inner-wrap m-2 clouds"
-                        : "weather-inner-wrap m-2 rain")
-                    }
-                  >
-                    {" "}
-                    <div className="day cabin"> {moment(timestamp.toDate(curr.dt)).format('DD/MM')} </div>
-                    {props.weather.data.daily &&
-                      capitalize(
-                        curr.weather[0].main
-                      )}{" "}
-                      <div className='temp cabin'>↓ {curr.temp.min}°  ↑ {curr.temp.max}°</div>
-                  </div>
-                 
-                  
-                </>
-              ))}
-               
-          </div>
-          <h1 className='cabin m-5'>Today:</h1>
-          <div className='d-flex flex-row flex-wrap'>
-          {props.weather.data.hourly && props.weather.data.hourly.slice(0,8).map((curr, index) => (
-                  
+          <div className="weather-wrap">
+            <h1 className='cabin mb-3'>Weather in {capitalize(props.city.city)}:</h1>
+            <div className="d-flex flex-row flex-wrap justify-content-center">
+              {props.weather.data.daily &&
+                props.weather.data.daily.map((curr) => (
+
                   <>
-                  {console.log(curr.temp.min)}
+
                     <div
                       className={
                         curr &&
                         (curr.weather[0].id >= 200 && curr.weather[0].id < 300
                           ? "weather-inner-wrap m-2 thunder"
                           : curr.weather[0].id >= 300 && curr.weather[0].id < 400
-                          ? "weather-inner-wrap m-2 drizzle"
-                          : curr.weather[0].id >= 400 && curr.weather[0].id < 600
-                          ? "weather-inner-wrap m-2 rain"
-                          : curr.weather[0].id >= 600 && curr.weather[0].id < 700
-                          ? "weather-inner-wrap m-2 snow"
-                          : curr.weather[0].id === 800
-                          ? "weather-inner-wrap m-2 clear"
-                          : curr.weather[0].id > 800
-                          ? "weather-inner-wrap m-2 clouds"
-                          : "weather-inner-wrap m-2 rain")
+                            ? "weather-inner-wrap m-2 drizzle"
+                            : curr.weather[0].id >= 400 && curr.weather[0].id < 600
+                              ? "weather-inner-wrap m-2 rain"
+                              : curr.weather[0].id >= 600 && curr.weather[0].id < 700
+                                ? "weather-inner-wrap m-2 snow"
+                                : curr.weather[0].id === 800
+                                  ? "weather-inner-wrap m-2 clear"
+                                  : curr.weather[0].id > 800
+                                    ? "weather-inner-wrap m-2 clouds"
+                                    : "weather-inner-wrap m-2 rain")
                       }
                     >
                       {" "}
-                      <div className="hour cabin">H.{moment(timestamp.toDate(curr.dt)).format('HH')} </div>
+                      <img className='icon' src={curr &&
+                        (curr.weather[0].id >= 200 && curr.weather[0].id < 300
+                          ? Thunder
+                          : curr.weather[0].id >= 300 && curr.weather[0].id < 400
+                            ? Rain
+                            : curr.weather[0].id >= 400 && curr.weather[0].id < 600
+                              ? Rain
+                              : curr.weather[0].id >= 600 && curr.weather[0].id < 700
+                                ? Snow
+                                : curr.weather[0].id === 800
+                                  ? Sun
+                                  : curr.weather[0].id > 800
+                                    ? Cloud
+                                    : Rain)} />
+                      <div className="day cabin"> {moment(timestamp.toDate(curr.dt)).format('DD/MM')} </div>
                       {props.weather.data.daily &&
                         capitalize(
                           curr.weather[0].main
                         )}{" "}
-                        <div className='temp cabin'>{curr.temp}°</div>
+                      <div className='temp cabin'>↓ {curr.temp.min}°  ↑ {curr.temp.max}°</div>
                     </div>
-                   
-                    
+
+
                   </>
                 ))}
 
-          </div>
+            </div>
+            <h1 className='cabin m-5'>Today:</h1>
+            <div className='d-flex flex-row flex-wrap justify-content-center'>
+              {props.weather.data.hourly && props.weather.data.hourly.slice(0, 8).map((curr, index) => (
 
-        </div>
-      )}
+                <>
+                  {console.log(curr.temp.min)}
+                  <div
+                    className={
+                      curr &&
+                      (curr.weather[0].id >= 200 && curr.weather[0].id < 300
+                        ? "weather-inner-wrap m-2 thunder"
+                        : curr.weather[0].id >= 300 && curr.weather[0].id < 400
+                          ? "weather-inner-wrap m-2 drizzle"
+                          : curr.weather[0].id >= 400 && curr.weather[0].id < 600
+                            ? "weather-inner-wrap m-2 rain"
+                            : curr.weather[0].id >= 600 && curr.weather[0].id < 700
+                              ? "weather-inner-wrap m-2 snow"
+                              : curr.weather[0].id === 800
+                                ? "weather-inner-wrap m-2 clear"
+                                : curr.weather[0].id > 800
+                                  ? "weather-inner-wrap m-2 clouds"
+                                  : "weather-inner-wrap m-2 rain")
+                    }
+                  >
+                    {" "}
+                    <img className='icon' src={curr &&
+                      (curr.weather[0].id >= 200 && curr.weather[0].id < 300
+                        ? Thunder
+                        : curr.weather[0].id >= 300 && curr.weather[0].id < 400
+                          ? Rain
+                          : curr.weather[0].id >= 400 && curr.weather[0].id < 600
+                            ? Rain
+                            : curr.weather[0].id >= 600 && curr.weather[0].id < 700
+                              ? Snow
+                              : curr.weather[0].id === 800
+                                ? Sun
+                                : curr.weather[0].id > 800
+                                  ? Cloud
+                                  : Rain)} />
+                    <div className="hour cabin">H.{moment(timestamp.toDate(curr.dt)).format('HH')} </div>
+                    {props.weather.data.daily &&
+                      capitalize(
+                        curr.weather[0].main
+                      )}{" "}
+                    <div className='temp cabin'>{curr.temp}°</div>
+                  </div>
+
+
+                </>
+              ))}
+
+            </div>
+
+          </div>
+        )}
     </div>
   );
 };
